40b1d9017f6781d15745c24c68dc95562118f33c
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-09-03 16:19:55 +0000

    # CINERINO-1 ### Added - 確認番号で注文検索を追加。

---
+++ example/findOrderByConfirmationNumber.js
@@ -0,0 +1,28 @@
+/**
+ * 確認番号で注文検索
+ */
+const auth = require('./auth');
+const client = require('../lib/index');
+
+async function main() {
+    const authClient = await auth.login();
+    await authClient.refreshAccessToken();
+    const loginTicket = authClient.verifyIdToken({});
+    console.log('username is', loginTicket.getUsername());
+
+    const orderService = new client.service.Order({
+        endpoint: process.env.API_ENDPOINT,
+        auth: authClient
+    });
+    const order = await orderService.findByConfirmationNumber({
+        confirmationNumber: 12345,
+        customer: {
+            telephone: '+819012345678'
+        }
+    });
+    console.log('order found', order.orderDate);
+}
+
+main().then(() => {
+    console.log('success!');
+}).catch(console.error);
