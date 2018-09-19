5b583fdd7d946aad7f51b4cf04afe2dea542a644
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-09-19 14:54:53 +0000

    # CINERINO-2 ### Changed - 注文検索条件を強化。

---
+++ example/searchOrders.js
@@ -0,0 +1,37 @@
+/**
+ * 注文検索
+ */
+const moment = require('moment');
+const auth = require('./authAsAdmin');
+const client = require('../lib/index');
+
+async function main() {
+    const authClient = await auth.login();
+    await authClient.refreshAccessToken();
+    const loginTicket = authClient.verifyIdToken({});
+    console.log('username is', loginTicket.getUsername());
+
+    const orderService = new client.service.Order({
+        endpoint: process.env.API_ENDPOINT,
+        auth: authClient
+    });
+
+    const { totalCount, data } = await orderService.search({
+        limit: 10,
+        page: 1,
+        orderDateFrom: moment().add(-1, 'month').toDate(),
+        orderDateThrough: moment().toDate(),
+        customer: {
+            typeOf: client.factory.personType.Person,
+            identifiers: [
+                { name: 'SampleName', value: 'SampleValue' }
+            ]
+        }
+    });
+    console.log(totalCount, 'orders found');
+    console.log(data.length, 'orders returned');
+}
+
+main().then(() => {
+    console.log('success!');
+}).catch(console.error);
