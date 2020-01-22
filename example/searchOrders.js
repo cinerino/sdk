432e4de420740918ef01258c9dd8396bc3592046
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-01-22 13:10:49 +0000

    ### Changed - 注文検索条件拡張 - 注文検索のX-Total-Count無効化対応

--- example/searchOrders.js
+++ example/searchOrders.js
@@ -13,59 +13,74 @@ async function main() {
 
     const orderService = new client.service.Order({
         endpoint: process.env.API_ENDPOINT,
-        auth: authClient
+        auth: authClient,
+        project: { id: 'oyatsu-test' }
     });
 
-    const { totalCount, data } = await orderService.search({
-        limit: 10,
-        page: 1,
-        orderDateFrom: moment().add(-1, 'week').toDate(),
-        orderDateThrough: moment().toDate(),
-        // identifier: {
-        //     $in: [
-        //         { name: 'sampleOrderIdentifierName1', value: 'sampleOrderIdentifiervalue1' },
-        //         { name: 'sampleOrderIdentifierName2', value: 'sampleOrderIdentifiervalue2' }
-        //     ]
-        // },
-        // customer: {
-        //     typeOf: client.factory.personType.Person,
-        //     identifiers: [
-        //         { name: 'SampleName', value: 'SampleValue' }
-        //     ],
-        //     telephone: '3896$'
-        // },
-        paymentMethods: {
-            // typeOfs: [client.factory.paymentMethodType.Account],
-            // paymentMethodIds: ['30118000911']
-        },
-        acceptedOffers: {
-            itemOffered: {
-                // ids: ['118-181018-000018-0'],
-                reservationFor: {
-                    // ids: ['xdflc2sjncbz279'],
-                    // name: '',
-                    // inSessionFrom: moment().add(1, 'days').toDate(),
-                    // inSessionThrough: moment().add(4, 'days').toDate(),
-                    // startFrom: moment().toDate(),
-                    // startThrough: moment().add(3, 'days').toDate(),
-                    location: {
-                        // branchCodes: ['90'],
+    const now = new Date();
+
+    let i = 0;
+    setInterval(
+        async () => {
+            i += 1;
+            try {
+
+                const { totalCount, data } = await orderService.search({
+                    limit: 10,
+                    page: 1,
+                    orderDateFrom: moment(now).add(-1, 'month').toDate(),
+                    orderDateThrough: now,
+                    // identifier: {
+                    //     $in: [
+                    //         { name: 'sampleOrderIdentifierName1', value: 'sampleOrderIdentifiervalue1' },
+                    //         { name: 'sampleOrderIdentifierName2', value: 'sampleOrderIdentifiervalue2' }
+                    //     ]
+                    // },
+                    // customer: {
+                    //     typeOf: client.factory.personType.Person,
+                    //     identifiers: [
+                    //         { name: 'SampleName', value: 'SampleValue' }
+                    //     ],
+                    //     telephone: '3896$'
+                    // },
+                    paymentMethods: {
+                        // typeOfs: [client.factory.paymentMethodType.Account],
+                        // paymentMethodIds: ['30118000911']
                     },
-                    superEvent: {
-                        // ids: ['405gzn58jnbd0wks'],
-                        location: {
-                            // branchCodes: ['002'],
-                        },
-                        workPerformed: {
-                            // identifiers: ['1622100'],
+                    acceptedOffers: {
+                        itemOffered: {
+                            // ids: ['118-181018-000018-0'],
+                            reservationFor: {
+                                // ids: ['xdflc2sjncbz279'],
+                                // name: '',
+                                // inSessionFrom: moment().add(1, 'days').toDate(),
+                                // inSessionThrough: moment().add(4, 'days').toDate(),
+                                // startFrom: moment().toDate(),
+                                // startThrough: moment().add(3, 'days').toDate(),
+                                location: {
+                                    // branchCodes: ['90'],
+                                },
+                                superEvent: {
+                                    // ids: ['405gzn58jnbd0wks'],
+                                    location: {
+                                        // branchCodes: ['002'],
+                                    },
+                                    workPerformed: {
+                                        // identifiers: ['1622100'],
+                                    }
+                                }
+                            }
                         }
                     }
-                }
+                });
+                console.log(i, totalCount, 'orders found');
+                console.log(i, data.length, 'orders returned');
+            } catch (error) {
+                console.error(error);
             }
-        }
-    });
-    console.log(totalCount, 'orders found');
-    console.log(data.length, 'orders returned');
+        },
+        500
+    );
 }
 
 main().then(() => {
